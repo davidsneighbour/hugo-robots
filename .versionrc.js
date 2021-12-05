6d120f64e4ec6ce1b26b190e3a890e882fb2b81a
Patrick Kollitsch <patrick@davids-neighbour.com>
2021-12-05 18:21:57 +0700

    build(fix): package.json version number not updating on release

--- .versionrc.js
+++ .versionrc.js
@@ -1,7 +1,10 @@
 const defaultStandardVersion = require("@dnb-org/standard-version-config");
 
 const localStandardVersion = {
-  bumpFiles: [{ filename: "data/dnb/robots/version.json", type: "json" }],
+  bumpFiles: [
+    ...defaultStandardVersion.bumpFiles,
+    { filename: "data/dnb/robots/version.json", type: "json" },
+  ],
 };
 
 module.exports = {
